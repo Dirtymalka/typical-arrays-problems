f11403c18edac92828839b466e2af239511dc787
Artem <artem.antonov1993@gmail.com>
2020-02-19 20:32:28 +0300

    feat: add solution of the task 'Typical Arrays Problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
+    if (arguments.length > 0 && array.length > 0) {
+        array.sort(function(a, b) { return a - b; });
+        console.log(array);
+        return array[0];
+    }
   return 0;
 }
 
 exports.max = function max (array) {
+    if (arguments.length > 0 && array.length > 0) {
+        array.sort(function(a, b) { return a - b; });
+        console.log(array);
+        return array[array.length - 1];
+    }
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if (arguments.length > 0 && array.length > 0) {
+        return (array.reduce((sum, current) => sum + current, 0)) / array.length;
+    }
   return 0;
 }
